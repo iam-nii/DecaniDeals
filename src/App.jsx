0cbd95b13f7a9dcc0b824a20baba210822c1b61c
adjeiboyejnr <adjeiboyejnr@gmail.com>
2024-02-19 20:01:59 +0300

    Create header component

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import About from './pages/About'
 import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import Header from './components/Header'
 
 export default function App() {
-  return <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home />}/>
-      <Route path='/about' element={<About />}/>
-      <Route path='/profile' element={<Profile />}/>
-      <Route path='/sign-in' element={<SignIn />}/>
-      <Route path='/sign-up' element={<SignUp />}/>
+  return(
+   <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/profile' element={<Profile />}/>
+        <Route path='/sign-in' element={<SignIn />}/>
+        <Route path='/sign-up' element={<SignUp />}/>
 
-    </Routes>
+      </Routes>
     </BrowserRouter>
+  )
   
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import {FaSearch} from 'react-icons/fa';
+import {Link} from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header className='bg-blue-200 shadow-md'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+            <Link to='/'>
+                <h1 className='font-bold text-md sm:text-xl flex flex-wrap'>
+                    <span className='text-slate-500'>Decani</span>
+                    <span className='text-slate-700'>Deals</span>
+                </h1>
+            </Link>
+            
+            <form className='bg-blue-100 p-3 rounded-lg flex items-center'>
+                <input type="text" placeholder='Search...' 
+                className='bg-transparent focus:outline-none w-24 sm:w-64' />
+                <FaSearch/>
+            </form>
+            <ul className='flex items-center justify-evenly gap-7 sm:gap-4'>
+                <Link to={'/'}>
+                    <li className='hidden sm:inline hover:underline'>Home</li>
+                </Link>
+                <Link to={'/about'}><li className='hidden sm:inline hover:underline'>About</li></Link>
+                <Link to={'/sign-in'}><li className=' hover:underline'>Sign in</li></Link>
+            </ul>
+        </div>
+        
+    </header>
+  )
+}
