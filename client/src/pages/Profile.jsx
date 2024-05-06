11b716735c7e6ac181803b3b21f1179778108924
adjeiboyejnr <adjeiboyejnr@gmail.com>
2024-05-06 11:30:08 +0300

    Complete show user listings funcitonality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,9 @@
 import {useSelector} from 'react-redux';
 import { useRef,useState,useEffect } from 'react';
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
-import {app} from '../firebase.js'
+import {app} from '../firebase.js';
 import {useDispatch} from 'react-redux';
-import {Link} from 'react-router-dom';
+import {Link, useAsyncError} from 'react-router-dom';
 import { deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserFailure, signOutUserStart, signOutUserSuccess, updateUserFailure, updateUserStart, updateUserSuccess } from '../redux/user/userSlice.js';
 
 
@@ -15,8 +15,15 @@ export default function Profile(){
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+
+
+
+  const [userListings,setUserListings] = useState([]);
+
+
   const dispatch = useDispatch();
 
   console.log(formData);
@@ -114,9 +121,28 @@ export default function Profile(){
     }
   }
 
+  const handleShowLisings = async ()=>{
+    setShowListingsError(false);
+    try {
+      const res =  await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if(data.success === false){
+        setShowListingsError(true);
+        return;
+      }
+      setUserListings(()=>{
+        return [...data["Film"], ...data["Bag"]];
+      });
+      console.log(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
-      <h1 className="font-semibold text-4xl text-center my-9">Profile</h1>
+      <h1 className="font-semibold text-4xl text-center my-7">Profile</h1>
 
       <form onSubmit={handleSubmit} action=""className='flex flex-col gap-4'>
       <img src={formData.avatar || currentUser.avatar} alt="profile" onClick={()=>fileRef.current.click()}
@@ -171,6 +197,33 @@ export default function Profile(){
       </div>  
       <p className='text-red-600 mt-4'>{error? error: ''}</p>
       <p className='text-green-600 mt-4'>{updateSuccess? 'User successfully updated': ''}</p>    
+
+      <button onClick={handleShowLisings} className='w-full text-green-400'>Show listings</button>
+      {showListingsError && <p className='text-red-600 mt-5'>Error showing listings</p>}
+      {
+        userListings && userListings.length > 1 && 
+        <div className='flex flex-col gap-4'>
+          <h1 className='font-semibold text-center mt-7 text-2xl'>Your listings</h1>
+          {userListings.map((listing)=>(
+          <div key={listing._id} className='p-3 border flex rounded-lg justify-between items-center'>
+            <Link to={`/listing/${listing._id}`}>
+              <img src={listing.imageUrls[0]} alt="listing cover" className='h-16 w-16 object-contain'/>
+            </Link>
+            <Link className='text-slate-700 font-semibold hover:underline flex-1 truncate' to={`/listing/${listing._id}`}>
+              <p >{listing.name}</p>
+            </Link>
+            <div className="flex flex-col justify-between items-end">
+              <button className='uppercase text-red-600'>Delete</button>
+              <button className='uppercase text-green-600'>Edit</button>
+            
+          </div>
+          </div>
+          
+          ))}
+
+        </div>
+         
+      }
     </div>
   )
 }
